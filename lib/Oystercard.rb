d5a0ddd9e4207d419004c4224d0d966d3954a9e0
Joseph <jpkershaw17@gmail.com>
2020-10-22 15:56:22 +0100

    Traking journey histories

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,10 +1,10 @@
 class Oystercard
-attr_reader :balance, :in_transit, :entry_station
+attr_reader :balance, :in_transit, :entry_station, :exit_station, :journey, :history
 CARD_LIMIT = 90
 MINIMUM_FARE = 1
   def initialize
     @balance = 0
-    @in_transit = false
+    @history = []
   end
   def top_up(amount)
     raise("#{CARD_LIMIT} limit reached!") if @balance + amount > CARD_LIMIT
@@ -13,13 +13,16 @@ MINIMUM_FARE = 1
   def touch_in(entry_station)
     raise("Insufficient funds, minimum £1") if @balance < MINIMUM_FARE
     @entry_station = entry_station
-    @in_transit = true
+    @journey = {}
   end
   def in_journey?
-    @in_transit
+    !!@entry_station
   end
-  def touch_out
-    @in_transit = false
+  def touch_out(exit_station)
+    @exit_station = exit_station
+    @journey[@entry_station] = @exit_station
+    # @History << @journey
+    @entry_station = nil
     @balance -= MINIMUM_FARE
   end
   private
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,9 +2,17 @@ require 'Oystercard'
 
 describe Oystercard do
   let (:entry_station) {double :entry_station}
-  it '#initialize: starting balance is 0' do
-    expect(subject.balance).to eq 0
-  end
+  let (:exit_station) {double :exit_station}
+    it '#initialize: starting balance is 0' do
+      expect(subject.balance).to eq 0
+    end
+    it 'do we have a History array' do
+      expect( subject.history ).to be_an_instance_of Array
+    end
+    it 'empty list of journeys by default' do
+      expect( subject.history ).to eq []
+    end
+
   describe '#top_up' do
     it 'balance increases by amount given' do
       initial_balance = subject.balance
@@ -34,7 +42,7 @@ describe Oystercard do
     it 'when you touch in you are in transit' do
       subject.top_up(Oystercard::MINIMUM_FARE)
       subject.touch_in(entry_station)
-      expect(subject.in_transit).to eq true
+      expect(subject.in_journey?).to eq true
     end
     it 'throw error for insufficient funds' do
       expect{ subject.touch_in(entry_station) }.to raise_error("Insufficient funds, minimum £1")
@@ -45,13 +53,30 @@ describe Oystercard do
       expect(subject.entry_station).to eq entry_station
     end
   end
-  it '#touch_out' do
-    subject.top_up(Oystercard::MINIMUM_FARE)
-    subject.touch_in(entry_station)
-    subject.touch_out
-    expect(subject.in_transit).to eq false
+  describe '#touch_out' do
+    it 'not in transit' do
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.in_journey?).to eq false
+    end
+    it 'sets entry_station to nil' do
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      subject.touch_in(entry_station)
+      subject.touch_out(exit_station)
+      expect(subject.entry_station).to eq nil
+    end
   end
   it 'a charge is made when touching out' do
-    expect{ subject.touch_out }.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+    subject.top_up(Oystercard::MINIMUM_FARE)
+    subject.touch_in(entry_station)
+    expect{ subject.touch_out(exit_station) }.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+  end
+  it 'touching in and out is 1 journey' do
+    subject.top_up(Oystercard::MINIMUM_FARE)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.journey.length).to eq 1
+
   end
 end
